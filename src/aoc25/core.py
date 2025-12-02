4073643455a25923275d4f649495a1cbd67fb8cf
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2025-12-02 01:23:30 +0000

    Update/day01 (#1)
    * Add pre-commit config
    
    * Run pre-commit
    
    * Add project config
    
    * Add core module
    
    * Add day01 tests
    
    * Add day01 solutions
    
    * Add main GitHub Action
    
    * Fix test data location
    
    * Add main file

---
+++ src/aoc25/core.py
@@ -0,0 +1,35 @@
+import os
+
+import aoc25
+from aoc25 import *
+
+
+def load_file(path):
+    with open(path) as file:
+        data = [line.strip() for line in file.readlines()]
+
+    return data
+
+
+class Solver:
+    def __init__(self):
+        self.data = {}
+        self.solutions = {}
+
+    def load_data(self, path):
+        files = os.listdir(path)
+
+        for file in files:
+            key = file.split(".")[0]
+            value = load_file(os.path.join(path, file))
+
+            self.data[key] = value
+
+    def run(self):
+        for n in self.data.keys():
+            module = getattr(aoc25, f"day{n}")
+
+            solution_part1 = module.part1(self.data[n])
+            solution_part2 = module.part2(self.data[n])
+
+            self.solutions[f"Day {n}"] = (solution_part1, solution_part2)
---
+++ tests/test_day01.py
@@ -0,0 +1,11 @@
+from aoc25 import core, day01
+
+data = core.load_file("./test_data/01.txt")
+
+
+def test_part1():
+    assert day01.part1(data) == 3
+
+
+def test_part2():
+    assert day01.part2(data) == 6
